c67b7dc277f8bae819e48727d546d3099dc66cba
Itachi727 <66966606+Itachi727@users.noreply.github.com>
2020-06-16 03:33:56 +0300

    Update chess.php

--- chess.php
+++ chess.php
@@ -4,7 +4,3 @@ session_start();
 
 if (isset($_GET['newFigures']))
   $_SESSION['map'] = 'rnbqkbnrpppppppp11111111111111111111111111111111PPPPPPPPRNBQKBNR';
-
-if (isset($_GET['newFigures']))
-  echo $_SESSION['map'];
-
