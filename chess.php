08ffc273db5fdaba75e20cb6b76ec6e13c28e768
Itachi727 <66966606+Itachi727@users.noreply.github.com>
2020-06-16 03:33:31 +0300

    Update chess.php

--- chess.php
+++ chess.php
@@ -8,11 +8,3 @@ if (isset($_GET['newFigures']))
 if (isset($_GET['newFigures']))
   echo $_SESSION['map'];
 
-if (isset($_GET['newFigure'])) {
-  $frCoord = $_GET['frCoord'];
-  $toCoord = $_GET['toCoord'];
-  $figure = $_SESSION['map'][$frCoord];
-  $_SESSION['map'][$frCoord] = '1';
-  $_SESSION['map'][$frCoord] = '$figures';
-  echo $_SESSION['map'];
-}
